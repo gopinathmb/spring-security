2cf51cdae5ae821455c98284fe69461766b8c54d
Gopinatha M B <gopinathamb@indeed.com>
2022-05-02 23:51:34 +0530

    Adding spring security with default username and password in application.properties

---
+++ spring-security-authentication/src/main/java/com/gopi/springsecurityauthentication/SpringSecurityAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.gopi.springsecurityauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationApplication.class, args);
+    }
+
+}
---
+++ spring-security-authentication/src/test/java/com/gopi/springsecurityauthentication/SpringSecurityAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopi.springsecurityauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
